87b4c47c5f437956c5d437f296a976e406bffd23
Ramtin Abolfazli <Ramtina1381@gmail.com>
2025-04-02 09:08:04 -0400

    Initialized angular applicaiton

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = '101419226_lab_test2_comp3133';
+}
